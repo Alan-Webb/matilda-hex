2b155a20596ea6883291687ccd4fe46bd360f901
Alan Webb <webbalan91939@gmail.com>
2024-09-27 20:09:08 +0100

    Update navbar styling & header text effect

--- js/script.js
+++ js/script.js
@@ -18,3 +18,16 @@ function userScroll() {
 }
 
 document.addEventListener("DOMContentLoaded", userScroll);
+
+// Replace text in header
+const checkReplace = document.querySelector(".replace-me");
+
+if (checkReplace !== null) {
+	const replace = new ReplaceMe(checkReplace, {
+		animation: "animated fadeIn",
+		speed: 2000,
+		separator: ",",
+		loopCount: "infinite",
+		autoRun: true,
+	});
+}
