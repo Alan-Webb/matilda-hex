cb73dcbd89b00cbc3cda48cb8a1a96a7cc764152
Alan Webb <webbalan91939@gmail.com>
2025-08-04 12:42:16 +0100

    Update Navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,14 @@
+import Logo from "../assets/matty-hex-logo.webp";
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div>
+			<nav className="border-b">
+				<a href="#">
+					<img src={Logo} alt="Matilda Hex Logo" className="w-[4rem] p-2" />
+				</a>
+			</nav>
+		</div>
+	);
 };
 export default Navbar;
