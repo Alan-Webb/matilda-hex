817352e7bb5875fd1c0c3d329138c777f83a4442
Alan Webb <webbalan91939@gmail.com>
2025-08-21 12:08:50 +0100

    Update assets and project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import {Route, Routes} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
+const App = () => {
+	return (
+		<main>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</main>
+	);
+};
+export default App;
