85c450d3703587ec718d0192fb6bf8a723826083
Alan Webb <webbalan91939@gmail.com>
2025-08-29 09:16:34 +0100

    Update Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,22 +2,21 @@ import {NavLink} from "react-router-dom";
 import {LINKS} from "../constants";
 
 const Navbar = () => {
-	// const NavLinks = [
-	// 	{path: "/", title: "Home"},
-	// 	{path: "/collection", title: "Collection"},
-	// 	{path: "/testimonials", title: "Testimonials"},
-	// 	{path: "/mailto:matildahex@gmail.com", title: "Contact"},
-	// ];
+	const NavLinks = [
+		{path: "/", title: "Home"},
+		{path: "/collection", title: "Collection"},
+		{path: "/testimonials", title: "Testimonials"},
+		{path: "/mailto:matildahex@gmail.com", title: "Contact"},
+	];
 
 	return (
 		<nav className="flex gap-x-6 mt-6 text-2xl">
-			{LINKS.map((link) => (
-				<NavLink>
-					<ul key={link.id}>
-						<li className="list-none hover:text-red-800" to={link.path}>
-							{link.title}
-						</li>
-					</ul>
+			{NavLinks.map((link) => (
+				<NavLink
+					key={link.title}
+					className="list-none hover:text-red-500"
+					to={link.path}>
+					{link.title}
 				</NavLink>
 			))}
 		</nav>
