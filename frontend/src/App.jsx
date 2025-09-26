2b6db3bf24405b68e51755f7c9ccc8429fb79ff2
Alan Webb <webbalan91939@gmail.com>
2025-09-26 06:17:19 +0100

    Create & update Login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import {Toaster} from "sonner";
 
 const App = () => {
@@ -10,7 +11,8 @@ const App = () => {
 			<Routes>
 				{/* User Layout */}
 				<Route path="/" element={<UserLayout />}>
-					<Route index element={<Home />}></Route>
+					<Route index element={<Home />} />
+					<Route path="login" element={<Login />} />
 				</Route>
 				<Route>{/* Admin Layout */}</Route>
 			</Routes>
