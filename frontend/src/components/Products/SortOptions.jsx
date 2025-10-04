01d466e4daa1411c648ca7b6e2fc55ad38931809
Alan Webb <webbalan91939@gmail.com>
2025-10-04 07:10:37 +0100

    Update SortOptions component

--- frontend/src/components/Products/SortOptions.jsx
+++ frontend/src/components/Products/SortOptions.jsx
@@ -1,5 +1,16 @@
 const SortOptions = () => {
-	return <div>SortOptions</div>;
+	return (
+		<div class="flex items-center justify-end">
+			<select
+				id="sort"
+				class="bg-transparent border-b p-1 mb-4 mr-4 text-xl focus:outline-none focus:bg-black">
+				<option value="">Default</option>
+				<option value="priceAsc">Price: Low to High</option>
+				<option value="priceDesc">Price: High to Low</option>
+				<option value="popular">Popular</option>
+			</select>
+		</div>
+	);
 };
 
 export default SortOptions;
