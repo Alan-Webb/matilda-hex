a391301ebd8cbc52535a9a0a9b3e40a0f49f49fe
Alan Webb <webbalan91939@gmail.com>
2025-10-06 10:09:50 +0100

    Update SortOptions component

--- frontend/src/components/Products/SortOptions.jsx
+++ frontend/src/components/Products/SortOptions.jsx
@@ -1,16 +1,20 @@
-import {useState} from "react";
+import {useSearchParams} from "react-router-dom";
 
 const SortOptions = () => {
-	const [searchParams, setSearchParams] = useState("");
+	const [searchParams, setSearchParams] = useSearchParams();
 
-	function handleSortChange() {}
+	function handleSortChange(e) {
+		const sortBy = e.target.value;
+		searchParams.set("sortBy", sortBy);
+		setSearchParams(searchParams);
+	}
 
 	return (
 		<div class="flex items-center justify-end">
 			<select
 				id="sort"
 				onChange={handleSortChange}
-				value={searchParams}
+				value={searchParams.get("sortBy") || ""}
 				class="bg-transparent border-b p-1 mb-4 mr-4 text-xl focus:outline-none focus:bg-black">
 				<option value="">Default</option>
 				<option value="priceAsc">Price: Low to High</option>
