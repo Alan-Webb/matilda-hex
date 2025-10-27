8e8b72a177d3c9db721d680cd4a3f0f81ae61742
Alan Webb <webbalan91939@gmail.com>
2025-10-27 10:47:05 +0000

    Update EditProductPage comp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import AdminLayout from "./components/admin/AdminLayout";
 import AdminHomePage from "./pages/AdminHomePage";
 import UserManagement from "./components/admin/UserManagement";
 import ProductManagement from "./components/admin/ProductManagement";
+import EditProductPage from "./components/admin/EditProductPage";
 
 const App = () => {
 	return (
@@ -38,6 +39,7 @@ const App = () => {
 					<Route index element={<AdminHomePage />} />
 					<Route path="users" element={<UserManagement />} />
 					<Route path="products" element={<ProductManagement />} />
+					<Route path="products/:id/edit" element={<EditProductPage />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
--- frontend/src/components/admin/EditProductPage.jsx
+++ frontend/src/components/admin/EditProductPage.jsx
@@ -1,5 +1,30 @@
+import {useState} from "react";
+
 const EditProductPage = () => {
-	return <div>EditProductPage</div>;
+	const [productData, setproductData] = useState({
+		name: "",
+		description: "",
+		price: 0,
+		countInStock: 0,
+		sku: "",
+		category: "",
+		invocationType: ["Blessing", "Hex", "Divination"],
+		expertiseLevel: ["Beginner", "Intermediate", "Expert"],
+		images: [
+			{
+				url: "https:picsum.photos.150?random=1",
+			},
+			{
+				url: "https:picsum.photos.150?random=2",
+			},
+		],
+	});
+
+	return (
+		<div className="max-w-8xl mx-auto p-6 absolute top-12 sm:left-28 md:left-72 xl:left-128">
+			EditProductPage
+		</div>
+	);
 };
 
 export default EditProductPage;
